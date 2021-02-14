ed94a4f897053f8a27be9d6ad9f712805066b54e
Okan <iokanyalcin@yandex.com>
2021-02-14 23:39:01 +0300

    Linear Combination designiing

--- LinAlgebra.py
+++ LinAlgebra.py
@@ -1,9 +1,37 @@
 import numpy as np
+
 class LinearAlgebra:
+    
     def __init__(self):
         pass
+    
+    @classmethod
+    def linear_combination(cls,c,vectors):
+        """
+        Computes the linear combination of vectors
+        param: c -- constants
+        param: vectors, a numpy array includes all vectors
+        """
 
-class Vector(LinearAlgebra):
+        assert type(c) == list, "constants must be stored in a list"
+        assert type(vectors) == list, "vectors must be stored in a list" 
 
-    def __init__(self):
-        super()
+        
+
+        
+    @classmethod
+    def help(cls):
+        print("This class designed for linear algebraic operations")
+
+    class vector:
+        def __init__(self,array):
+            #TODO: Create a vector
+            self.array = np.array(array)
+            self.shape = self.array.shape
+
+
+        
+
+            
+LinearAlgebra.help()
+LinearAlgebra.linear_combination([1],[2])
